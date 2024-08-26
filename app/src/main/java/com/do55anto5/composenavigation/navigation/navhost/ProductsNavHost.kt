82233015e766111d7154ec1726fbecba937ee2d3
do5-5anto5 <dos.santos.2k23@gmail.com>
2024-08-26 19:44:21 -0300

    Feat: Implement ProductsNavHost - This composable set routes to destinations, later it can handle arguments between screens

---
+++ app/src/main/java/com/do55anto5/composenavigation/navigation/navhost/ProductsNavHost.kt
@@ -0,0 +1,22 @@
+package com.do55anto5.composenavigation.navigation.navhost
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.do55anto5.composenavigation.navigation.routes.ProductRoutes.ProductDetailsScreenRoute
+import com.do55anto5.composenavigation.navigation.routes.ProductRoutes.ProductListScreenRoute
+import com.do55anto5.composenavigation.screens.ProductDetailsScreen
+import com.do55anto5.composenavigation.screens.ProductListScreen
+
+@Composable
+fun ProductsNavHost(navHostController: NavHostController) {
+    NavHost(navController = navHostController, startDestination = ProductListScreenRoute) {
+        composable<ProductListScreenRoute> {
+            ProductListScreen()
+        }
+        composable<ProductDetailsScreenRoute> {
+            ProductDetailsScreen()
+        }
+    }
+}
