e23fc050edfc15334b2c09643a47b2daed85daaa
do5-5anto5 <dos.santos.2k23@gmail.com>
2024-08-26 20:23:02 -0300

    Feat: implement Button Close app

--- app/src/main/java/com/do55anto5/composenavigation/screens/ProductListScreen.kt
+++ app/src/main/java/com/do55anto5/composenavigation/screens/ProductListScreen.kt
@@ -1,5 +1,6 @@
 package com.do55anto5.composenavigation.screens
 
+import androidx.activity.ComponentActivity
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -11,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -20,6 +22,9 @@ import androidx.compose.ui.unit.sp
 fun ProductListScreen(
     navigateToProductDetailsScreen: () -> Unit
     ) {
+
+    val context = LocalContext.current
+    val activity = context as? ComponentActivity
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -44,6 +49,23 @@ fun ProductListScreen(
                 content = {
                     Text(
                         text = "Go to Details",
+                        style = TextStyle(
+                            color = Color(0xFF2D60B9),
+                            fontSize = 20.sp,
+                            fontWeight = FontWeight.Bold
+                        )
+                    )
+                }
+            )
+
+            Button(
+                onClick = { activity?.finish() },
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.White
+                ),
+                content = {
+                    Text(
+                        text = "Close app",
                         style = TextStyle(
                             color = Color(0xFF2D60B9),
                             fontSize = 20.sp,
