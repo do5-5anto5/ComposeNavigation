b7042c045f8aa7d86efeba2d3688029ad3c0a1dc
do5-5anto5 <dos.santos.2k23@gmail.com>
2024-08-26 22:13:53 -0300

    Feat: handle arguments inside ProductsNavHost: - get productName from ProductListScreen then pass it to Details Screen route

--- app/src/main/java/com/do55anto5/composenavigation/navigation/navhost/ProductsNavHost.kt
+++ app/src/main/java/com/do55anto5/composenavigation/navigation/navhost/ProductsNavHost.kt
@@ -4,6 +4,7 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.toRoute
 import com.do55anto5.composenavigation.navigation.routes.ProductRoutes.ProductDetailsScreenRoute
 import com.do55anto5.composenavigation.navigation.routes.ProductRoutes.ProductListScreenRoute
 import com.do55anto5.composenavigation.screens.ProductDetailsScreen
@@ -14,13 +15,15 @@ fun ProductsNavHost(navHostController: NavHostController) {
     NavHost(navController = navHostController, startDestination = ProductListScreenRoute) {
         composable<ProductListScreenRoute> {
             ProductListScreen(
-                navigateToProductDetailsScreen = {
-                    navHostController.navigate(ProductDetailsScreenRoute)
+                navigateToProductDetailsScreen = { productName ->
+                    navHostController.navigate(ProductDetailsScreenRoute(name = productName))
                 }
             )
         }
-        composable<ProductDetailsScreenRoute> {
+        composable<ProductDetailsScreenRoute> { backStackEntry ->
+            val product: ProductDetailsScreenRoute = backStackEntry.toRoute()
             ProductDetailsScreen(
+                name = product.name,
                 onBtnBackPressed = {
                     navHostController.popBackStack()
                 }
