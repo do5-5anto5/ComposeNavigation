c61059b61f02c386ef2ef0c4066f8d394abe0d62
do5-5anto5 <dos.santos.2k23@gmail.com>
2024-08-28 07:23:57 -0300

    Feat: render description sent back by ProductDetailsScreen

--- app/src/main/java/com/do55anto5/composenavigation/screens/list/ProductListScreen.kt
+++ app/src/main/java/com/do55anto5/composenavigation/screens/list/ProductListScreen.kt
@@ -5,8 +5,10 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -15,12 +17,15 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.do55anto5.composenavigation.screens.details.ProductDetailsParameter
 
 @Composable
 fun ProductListScreen(
+    description: String? = null,
     navigateToProductDetailsScreen: (ProductDetailsParameter) -> Unit
 ) {
 
@@ -68,6 +73,21 @@ fun ProductListScreen(
                     )
                 }
             )
+
+            HorizontalDivider(modifier = Modifier.padding(vertical = 32.dp))
+            if (description != null) {
+                Text(
+                    text = description,
+                    style = TextStyle(
+                        color = Color.White,
+                        fontSize = 22.sp,
+                        fontWeight = FontWeight.Bold,
+                        textAlign = TextAlign.Center
+                    )
+                )
+            }
+
+            HorizontalDivider(modifier = Modifier.padding(vertical = 32.dp))
 
             Button(
                 onClick = { activity?.finish() },
