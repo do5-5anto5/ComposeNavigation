4ef5ea1c1581e268b8f77c87c90b688eb12988e5
do5-5anto5 <dos.santos.2k23@gmail.com>
2024-08-28 07:22:46 -0300

    Feat: set origin of argument in ProductDetailsScreen - it is passed when click in "back with description" button, and when press default android back button

--- app/src/main/java/com/do55anto5/composenavigation/screens/details/ProductDetailsScreen.kt
+++ app/src/main/java/com/do55anto5/composenavigation/screens/details/ProductDetailsScreen.kt
@@ -1,13 +1,21 @@
 package com.do55anto5.composenavigation.screens.details
 
+import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -15,13 +23,26 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @Composable
 fun ProductDetailsScreen(
     parameter: ProductDetailsParameter,
+    onBtnBackResult: (String) -> Unit,
     onBtnBackPressed: () -> Unit = {}
 ) {
+
+    var description by remember { mutableStateOf("") }
+
+    BackHandler {
+        if (description.isNotEmpty()) {
+            onBtnBackResult(description)
+        } else {
+            onBtnBackPressed()
+        }
+    }
+
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -74,6 +95,35 @@ fun ProductDetailsScreen(
                     )
                 }
             )
+
+            HorizontalDivider(
+                modifier = Modifier.padding(vertical = 32.dp),
+            )
+
+            TextField(
+                value = description,
+                onValueChange = {
+                    description = it
+                }
+            )
+
+            Button(
+                onClick = { onBtnBackResult(description) },
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.White
+                ),
+                content = {
+                    Text(
+                        text = "Navigate back with description",
+                        style = TextStyle(
+                            color = Color(0xFF2D60B9),
+                            fontSize = 20.sp,
+                            fontWeight = FontWeight.Bold
+                        )
+                    )
+                }
+            )
+
         }
     )
 }
@@ -82,6 +132,7 @@ fun ProductDetailsScreen(
 @Composable
 private fun ProductDetailsScreenPreview() {
     ProductDetailsScreen(
+        onBtnBackResult = {},
         parameter = ProductDetailsParameter(0, "", 0.0)
     )
 }
